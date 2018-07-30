b6d469d17c29570dd2d44d93d0c7a04b73375714
kumar-shridhar <shridhar.stark@gmail.com>
2018-07-30 14:50:20 +0200

    removed self-defined CNN (performs poorly)

--- MLEConvmodels.py
+++ MLEConvmodels.py
@@ -65,57 +65,6 @@ class _3Conv3FC(nn.Module):
         return x
 
 
-# Bayesian self-defined CNN
-class CNN1(nn.Module):
-    """
-    To train on CIFAR-100
-    """
-    def __init__(self, outputs, inputs):
-        super(CNN1, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(inputs, 92, 3, stride=1),
-            nn.Softplus(),
-            nn.MaxPool2d(kernel_size=2, stride=1),
-            nn.Conv2d(92, 384, 1, stride=1),
-            nn.Softplus(),
-            nn.Conv2d(384, 384, 2, stride=1),
-            nn.Softplus(),
-            nn.MaxPool2d(kernel_size=2, stride=1),
-            nn.Conv2d(384, 640, 2, stride=1),
-            nn.Softplus(),
-            nn.Conv2d(640, 640, 2, stride=1),
-            nn.Softplus(),
-            nn.MaxPool2d(kernel_size=2, stride=1),
-            nn.Conv2d(640, 640, 1, stride=1),
-            nn.Softplus(),
-            nn.Conv2d(640, 768, 2, stride=1),
-            nn.Softplus(),
-            nn.MaxPool2d(kernel_size=2, stride=1),
-            nn.Conv2d(768, 768, 2, stride=1),
-            nn.Softplus(),
-            nn.Conv2d(768, 768, 2, stride=1),
-            nn.Softplus(),
-            nn.MaxPool2d(kernel_size=2, stride=1),
-            nn.Conv2d(768, 768, 1, stride=1),
-            nn.Softplus(),
-            nn.Conv2d(768, 640, 2, stride=1),
-            nn.Softplus(),
-            nn.Conv2d(640, 384, 2, stride=1),
-            nn.Softplus(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.classifier = nn.Sequential(
-            FlattenLayer(8 * 8 * 384),
-            nn.Linear(8 * 8 * 384, outputs)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
-        print('X', x)
-        return x
-
-
 # ELU-Network 1
 class ELUN1(nn.Module):
     """

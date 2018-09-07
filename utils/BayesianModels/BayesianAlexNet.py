09c86a32b10804fbccdcaf4380b193b38f0c4fbf
kumar-shridhar <shridhar.stark@gmail.com>
2018-09-07 20:56:24 +0200

    updated results and files in utils

--- utils/BayesianModels/BayesianAlexNet.py
+++ utils/BayesianModels/BayesianAlexNet.py
@@ -1,33 +1,37 @@
-# imports
 
 import torch.nn as nn
 from utils.BBBlayers import BBBConv2d, BBBLinearFactorial, FlattenLayer
 
 
-class BBBLeNet(nn.Module):
-    '''The architecture of LeNet with Bayesian Layers'''
+class BBBAlexNet(nn.Module):
+    '''The architecture of AlexNet with Bayesian Layers'''
 
     def __init__(self, outputs, inputs):
-        super(BBBLeNet, self).__init__()
-        self.conv1 = BBBConv2d(inputs,6, 5, stride=1)
+        super(BBBAlexNet, self).__init__()
+        self.conv1 = BBBConv2d(inputs, 64, kernel_size=11, stride=4, padding=5)
         self.soft1 = nn.Softplus()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = BBBConv2d(6, 16, 5, stride=1)
+        self.conv2 = BBBConv2d(64, 192, kernel_size=5, padding=2)
         self.soft2 = nn.Softplus()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.flatten = FlattenLayer(5 * 5 * 16)
-        self.fc1 = BBBLinearFactorial(5 * 5 * 16, 120)
+        self.conv3 = BBBConv2d(192, 384, kernel_size=3, padding=1)
         self.soft3 = nn.Softplus()
 
-        self.fc2 = BBBLinearFactorial(120, 84)
+        self.conv4 = BBBConv2d(384, 256, kernel_size=3, padding=1)
         self.soft4 = nn.Softplus()
 
-        self.fc3 = BBBLinearFactorial(84, outputs)
+        self.conv5 = BBBConv2d(256, 128, kernel_size=3, padding=1)
+        self.soft5 = nn.Softplus()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        layers = [self.conv1, self.soft1, self.pool1, self.conv2, self.soft2, self.pool2,
-                  self.flatten, self.fc1, self.soft3, self.fc2, self.soft4, self.fc3]
+        self.flatten = FlattenLayer(1 * 1 * 128)
+        self.fc1 = BBBLinearFactorial(1* 1 * 128, outputs)
+
+
+        layers = [self.conv1, self.soft1, self.pool1, self.conv2, self.soft2, self.pool2, self.conv3, self.soft3,
+                  self.conv4, self.soft4, self.conv5, self.soft5, self.pool3, self.flatten, self.fc1]
 
         self.layers = nn.ModuleList(layers)
 
